f3a8cea1d2042b6568ef40199398a6398537f78f
Nikolay <nikolay56615@mail.ru>
2021-03-07 18:45:22 +0500

    Работающая регистрация, где отображается индекс

--- data/users.py
+++ data/users.py
@@ -1,13 +1,23 @@
 import sqlalchemy
+from flask_login import UserMixin
 from .db_session import SqlAlchemyBase
 
-class User(SqlAlchemyBase):
+
+class User(SqlAlchemyBase, UserMixin):
     __tablename__ = 'users'
 
-    username = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    username = sqlalchemy.Column(sqlalchemy.Integer, unique=True)
     email = sqlalchemy.Column(sqlalchemy.String, unique=True, nullable=True)
     hashed_password = sqlalchemy.Column(sqlalchemy.String, nullable=True)
 
     def __repr__(self):
         return ' '.join(
             [self.username, self.email, self.hashed_password])
+
+    def check_password(self, trying_pass):
+        if trying_pass == self.hashed_password:
+            return True
+        else:
+            return False
--- main.py
+++ main.py
@@ -1,39 +1,92 @@
-from flask import Flask, render_template, redirect
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired
+from flask_login import LoginManager, login_user, login_required, logout_user
 from data import db_session
 from data.users import User
+import datetime
+from flask import Flask, redirect, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+
+
+class RegFrom(FlaskForm):
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    repeat_password = PasswordField('Повторите пароль', validators=[DataRequired()])
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    submit = SubmitField('Зарегистрироваться')
 
 
 class LoginForm(FlaskForm):
-    asrt_id = StringField('login', validators=[DataRequired()])
-    name = StringField('username', validators=[DataRequired()])
-    asrt_pass = PasswordField('password', validators=[DataRequired()])
-    submit = SubmitField('Join')
-
-
-def main():
-    db_session.global_init("db/blogs.db")
-    db_sess = db_session.create_session()
-
-
-    app.run()
+    email = StringField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
 
 
 app = Flask(__name__)
+app.config['PERMANENT_SESSION_LIFETIME'] = datetime.timedelta(
+    days=365
+)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
 @app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_session.global_init("db/blogs.db")
+    db_sess = db_session.create_session()
+    return db_sess.query(User).get(user_id)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reg():
+    form = RegFrom()
+    if form.validate_on_submit():
+        db_session.global_init("db/blogs.db")
+        db_sess = db_session.create_session()
+        user = User()
+        if form.password.data == form.repeat_password.data:
+            user.email = form.email.data
+            user.hashed_password = form.password.data
+            user.username = form.username.data
+            db_sess.add(user)
+            db_sess.commit()
+            return redirect('/login')
+        return render_template('registration.html',
+                               message="пароли не совпадают",
+                               form=form)
+    return render_template('registration.html', title='Регистрация', form=form)
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        return redirect('/success')
+        db_session.global_init("db/blogs.db")
+        db_sess = db_session.create_session()
+        user = db_sess.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
     return render_template('login.html', title='Авторизация', form=form)
 
 
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
 if __name__ == '__main__':
-    main()
     app.run(port=8080, host='127.0.0.1')
